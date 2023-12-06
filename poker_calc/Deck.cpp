003eb6af330f522f5a0b5c42db5eb63d382091cf
Bryce <byoung@uccs.edu>
2023-12-06 01:50:25 -0700

    Messed around with some stuff, lets go to bed now

--- poker_calc/Deck.cpp
+++ poker_calc/Deck.cpp
@@ -3,63 +3,6 @@
 
 namespace sim
 {
-    Deck::Deck()
-    {
-        ReorderAndReset();
-    }
-    
-    void Deck::ShuffleAndReset(int nTimes, Random& random)
-    {
-        for (int i = 0; i < nTimes; i++)
-        {
-            const int card1Idx = random.NextInt(NumCardsInDeck);
-            const int card2Idx = random.NextInt(NumCardsInDeck);
-
-            // Here im not worried if the cards swap with each other. That's just part of the randomness.
-            const card_t tmpCard1 = mCards[card1Idx];
-            mCards[card1Idx] = mCards[card2Idx];
-            mCards[card2Idx] = tmpCard1;
-        }
-        
-        Reset();
-    }
-    
-    void Deck::ReorderAndReset() 
-    {
-        int deckIndex = 0;
-
-        // Create the deck of cards, each card should be in order.
-        for (int i = 0; i < static_cast<int>(Rank::MaxRank); i++)
-        {
-            for (int j = 0; j < static_cast<int>(Suit::MaxSuit); j++)
-            {
-                const card_t nextCard = Card::CreateCard(static_cast<Rank>(i), static_cast<Suit>(j));
-                mCards[deckIndex] = nextCard;
-                deckIndex++;
-            }
-        }
-        
-        Reset();
-    }
-
-    void Deck::Reset()
-    {
-        mTopOfDeckIndex = NumCardsInDeck - 1;
-    }
-    
-    card_t Deck::Draw()
-    {
-        if (mTopOfDeckIndex < 0) 
-        {
-            return NotACard;
-        }
-
-        const card_t nextCard = mCards[mTopOfDeckIndex];
-        mTopOfDeckIndex--;
-        
-        return nextCard;
-    }
-    
     void Deck::Print(int numCards, std::ostream& outputBuffer) const
     {
         outputBuffer << "Printing " << numCards << " from the top of the deck\n";
--- poker_calc/include/Deck.h
+++ poker_calc/include/Deck.h
@@ -12,22 +12,75 @@ namespace sim
     class Deck
     {
         public:
-            Deck();
+            Deck()
+            {
+                mCards = (card_t*)malloc(sizeof(card_t) * 128);
+                ReorderAndReset();
+            }
+
+            virtual ~Deck()
+            {
+                free(mCards);
+            }
             
             /// @brief Shuffles the cards and resets the deck.
             /// @param nTimes 
             /// @param random 
-            void ShuffleAndReset(int nTimes, Random& random);
+            inline void ShuffleAndReset(int nTimes, Random& random)
+            {
+                for (int i = 0; i < nTimes; ++i)
+				{
+					const int card1Idx = random.NextInt(NumCardsInDeck);
+					const int card2Idx = random.NextInt(NumCardsInDeck);
+
+					// Here im not worried if the cards swap with each other. That's just part of the randomness.
+					const card_t tmpCard1 = mCards[card1Idx];
+					mCards[card1Idx] = mCards[card2Idx];
+					mCards[card2Idx] = tmpCard1;
+				}
+
+				Reset();
+            }
             
             /// @brief Puts the cards back into the starting order.
-            void ReorderAndReset();
+            inline void ReorderAndReset()
+            {
+				int deckIndex = 0;
+
+				// Create the deck of cards, each card should be in order.
+				for (int i = 0; i < static_cast<int>(Rank::MaxRank); i++)
+				{
+					for (int j = 0; j < static_cast<int>(Suit::MaxSuit); j++)
+					{
+						const card_t nextCard = Card::CreateCard(static_cast<Rank>(i), static_cast<Suit>(j));
+						mCards[deckIndex] = nextCard;
+						deckIndex++;
+					}
+				}
+
+				Reset();
+            }
 
             /// @brief Returns all the cards to the deck in the last order they were seen in.
-            void Reset();
+            inline void Reset()
+            {
+				mTopOfDeckIndex = NumCardsInDeck - 1;
+            }
             
             /// @brief Draws a single card from the deck if possible.
             /// @return 0 if no cards are left.
-            card_t Draw();
+            inline card_t Draw()
+            {
+				if (mTopOfDeckIndex < 0)
+				{
+					return NotACard;
+				}
+
+				const card_t nextCard = mCards[mTopOfDeckIndex];
+				mTopOfDeckIndex--;
+
+				return nextCard;
+            }
             
             /// @brief Prints the top n cards from the deck
             /// @param numCards the number of cards to print
@@ -42,11 +95,10 @@ namespace sim
             /// random card in the deck is 51/52.
             /// (1/2) * log(10; 52) * 52 =/ 45;
             /// So to perform the shuffle, we will swap 45 random cards by default.
-            static constexpr int OptimalShuffleCount = 45;
-
+            static constexpr int OptimalShuffleCount = 28;
 
         private:
-            card_t mCards[NumCardsInDeck];
+            card_t* mCards;
             int mTopOfDeckIndex;
     };
 }
